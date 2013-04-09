0cb7e21dbc966a2452104639c944fe78ee6edf56
Sam Popowich <spopowich@gmail.com>
2013-04-09 13:38:42 -0600

    Initial commit.

---
+++ app/controllers/catalog_controller.rb
@@ -0,0 +1,241 @@
+# -*- encoding : utf-8 -*-
+require 'blacklight/catalog'
+
+class CatalogController < ApplicationController  
+
+  include Blacklight::Catalog
+
+  configure_blacklight do |config|
+    ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
+    config.default_solr_params = { 
+      :qt => 'search',
+      :rows => 10 
+    }
+
+    ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
+    ## parameters included in the Blacklight-jetty document requestHandler.
+    #
+    #config.default_document_solr_params = {
+    #  :qt => 'document',
+    #  ## These are hard-coded in the blacklight 'document' requestHandler
+    #  # :fl => '*',
+    #  # :rows => 1
+    #  # :q => '{!raw f=id v=$id}' 
+    #}
+
+    # solr field configuration for search results/index views
+    config.index.show_link = 'titleExact'
+    config.index.record_display_type = 'format'
+
+    # solr field configuration for document/show views
+    config.show.html_title = 'titleExact'
+    config.show.heading = 'titleExact'
+
+    # solr fields that will be treated as facets by the blacklight application
+    #   The ordering of the field names is the order of the display
+    #
+    # Setting a limit will trigger Blacklight's 'more' facet values link.
+    # * If left unset, then all facet values returned by solr will be displayed.
+    # * If set to an integer, then "f.somefield.facet.limit" will be added to
+    # solr request, with actual solr request being +1 your configured limit --
+    # you configure the number of items you actually want _displayed_ in a page.    
+    # * If set to 'true', then no additional parameters will be sent to solr,
+    # but any 'sniffed' request limit parameters will be used for paging, with
+    # paging at requested limit -1. Can sniff from facet.limit or 
+    # f.specific_field.facet.limit solr request params. This 'true' config
+    # can be used if you set limits in :default_solr_params, or as defaults
+    # on the solr side in the request handler itself. Request handler defaults
+    # sniffing requires solr requests to be made with "echoParams=all", for
+    # app code to actually have it echo'd back to see it.  
+    #
+    # :show may be set to false if you don't want the facet to be drawn in the 
+    # facet bar
+    #config.add_facet_field 'format', :label => 'Format'
+    #config.add_facet_field 'pub_date', :label => 'Publication Year', :single => true
+    #config.add_facet_field 'subject_topic_facet', :label => 'Topic', :limit => 20 
+    #config.add_facet_field 'language_facet', :label => 'Language', :limit => true 
+    #config.add_facet_field 'lc_1letter_facet', :label => 'Call Number' 
+    #config.add_facet_field 'subject_geo_facet', :label => 'Region' 
+    #config.add_facet_field 'subject_era_facet', :label => 'Era'  
+
+    #config.add_facet_field 'example_pivot_field', :label => 'Pivot Field', :pivot => ['format', 'language_facet']
+
+    #config.add_facet_field 'example_query_facet_field', :label => 'Publish Date', :query => {
+     #  :years_5 => { :label => 'within 5 Years', :fq => "pub_date:[#{Time.now.year - 5 } TO *]" },
+     #  :years_10 => { :label => 'within 10 Years', :fq => "pub_date:[#{Time.now.year - 10 } TO *]" },
+     #  :years_25 => { :label => 'within 25 Years', :fq => "pub_date:[#{Time.now.year - 25 } TO *]" }
+    #}
+
+    config.add_facet_field 'okinsirsi', :label=>'Monograph Serials'
+    config.add_facet_field 'multiCatKey', :label=>'Multiple Catkeys'
+    config.add_facet_field 'singleTarget', :label=>'Single Target'
+    config.add_facet_field 'cat', :label=>'Main Topic'
+    config.add_facet_field 'subcat', :label=>'Subtopic'
+    config.add_facet_field 'language', :label=>'Language'
+    config.add_facet_field 'freeJournal', :label=>'Free'
+    config.add_facet_field 'target', :label=>'Targets'
+    config.add_facet_field 'sirsiPubDateNotes', :label=>'Sirsi Date Notes'
+    config.add_facet_field 'inSFX', :label=>'In SFX Only'
+    config.add_facet_field 'inSirsi', :label=>'In Sirsi Only'
+
+
+    # Have BL send all facet field names to Solr, which has been the default
+    # previously. Simply remove these lines if you'd rather use Solr request
+    # handler defaults, or have no facets.
+    config.add_facet_fields_to_solr_request!
+
+    # solr fields to be displayed in the index (search results) view
+    #   The ordering of the field names is the order of the display 
+    #config.add_index_field 'title_display', :label => 'Title:' 
+    #config.add_index_field 'title_vern_display', :label => 'Title:' 
+    #config.add_index_field 'author_display', :label => 'Author:' 
+    #config.add_index_field 'author_vern_display', :label => 'Author:' 
+    #config.add_index_field 'format', :label => 'Format:' 
+    #config.add_index_field 'language_facet', :label => 'Language:'
+    #config.add_index_field 'published_display', :label => 'Published:'
+    #config.add_index_field 'published_vern_display', :label => 'Published:'
+    #config.add_index_field 'lc_callnum_display', :label => 'Call number:'
+    #config.add_index_field 'catkey', :label=>'CatKey: '
+    config.add_index_field 'title', :label=>'Title: '
+    config.add_index_field 'id', :label=>'SFX Object ID: '
+    config.add_index_field 'issn', :label=>'ISSN: '
+
+
+
+    # solr fields to be displayed in the show (single result) view
+    #   The ordering of the field names is the order of the display 
+    #config.add_show_field 'title_display', :label => 'Title:' 
+    #config.add_show_field 'title_vern_display', :label => 'Title:' 
+    #config.add_show_field 'subtitle_display', :label => 'Subtitle:' 
+    #config.add_show_field 'subtitle_vern_display', :label => 'Subtitle:' 
+    #config.add_show_field 'author_display', :label => 'Author:' 
+    #config.add_show_field 'author_vern_display', :label => 'Author:' 
+    #config.add_show_field 'format', :label => 'Format:' 
+    #config.add_show_field 'url_fulltext_display', :label => 'URL:'
+    #config.add_show_field 'url_suppl_display', :label => 'More Information:'
+    #config.add_show_field 'language_facet', :label => 'Language:'
+    #config.add_show_field 'published_display', :label => 'Published:'
+    #config.add_show_field 'published_vern_display', :label => 'Published:'
+    #config.add_show_field 'lc_callnum_display', :label => 'Call number:'
+    #config.add_show_field 'isbn_t', :label => 'ISBN:'
+    config.add_show_field 'id', :label=>'SFX Object ID: '
+    config.add_show_field 'catkey', :label=>'Catkey: '
+    config.add_show_field 'issn', :label=>'ISSN: '
+    config.add_show_field 'issnPrint', :label=>'Print ISSN: '
+    config.add_show_field 'issnElectronic', :label=>'Electronic ISSN: '
+    config.add_show_field 'issnINCORRECT', :label=>'Incorrect ISSN: '
+    config.add_show_field 'target', :label=>'Targets: '
+    config.add_show_field 'inSFX', :label=>'In SFX? '
+    config.add_show_field 'inSirsi', :label=>'In Sirsi? '
+    config.add_show_field 'singleTarget', :label=>'Single Target? '
+    config.add_show_field 'previousTitle', :label=>'Previous Title: '
+    config.add_show_field 'laterTitle', :label=>'Later Title: '
+
+    # config.add_show_field 'marcFetched', :label=>'MARC Fetched? ' # not sure what this even is
+    # config.add_show_field 'sirsiUrl', :label=>'Sirsi URL: ' # necessary?
+    # config.add_show_field 'sirsiDomain', :label=>'Sirsi Domain: '  # necessary?
+    # this would be easiest to pull from Jeremy's script as the complete date statement
+    #config.add_show_field 'from-year', :label=>'Start Year: '
+    #config.add_show_field 'from-volume', :label=>'Start Volume: '
+    #config.add_show_field 'from-issue', :label=>'Start Issue: '
+    #config.add_show_field 'to-year', :label=>'End Year: '
+    #config.add_show_field 'to-volume', :label=>'End Volume: '
+    #config.add_show_field 'to-issue', :label=>'End Issue: '
+    #config.add_show_field 'embargo-days', :label=>'Embargo: '
+    #config.add_show_field 'sirsiTitleExact', :label=>'SirsiTitle: ' # not sure where to get this data
+    #config.add_show_field 'sirsiHoldings', :label=>'Sirsi Holdings: ' # not sure where to get this data
+    config.add_show_field 'pubDateNotes', :label=>'Notes: '
+    #config.add_show_field 'cat', :label=>'Cat: '
+    #config.add_show_field 'subcat', :label=>'SubCat: '
+    #config.add_show_field 'catText', :label=>'Cat Text: '
+    #config.add_show_field 'subcatText', :label=>'SubCat Text: '
+    #config.add_show_field 'ejdb', :label=>'EJDB: '  # from the e-journals database. Necessary?
+    #config.add_show_field 'peerReviewed', :label=>'Peer Reviewed? ' # from the e-journals database. Necessary?
+    config.add_show_field 'language', :label=>'Language: '
+    #config.add_show_field 'sirsiMatchSource', :label=>'Sirsi Match Source: '
+    #config.add_show_field 'sirsiMatchDate', :label=>'Sirsi Match Date: '
+    config.add_show_field 'status', :label=>'Status: '
+    config.add_show_field 'freeJournal', :label=>'Free? '
+    config.add_show_field 'placeExact', :label=>'Place of Publication: '
+    config.add_show_field 'publisherExact', :label=>'Publisher: '
+    # config.add_show_field 'sfxHoldings', :label=>'SFX Holdings: ' Not sure what this is supposed to be. This is actually the dateStatement
+    config.add_show_field 'dateStatement', :label=>'Coverage: '
+    config.add_show_field 'alternateCatKey', :label=>'Alternate Catkey: '
+    config.add_show_field 'lastUpdated', :label=>'Last Updated: '
+
+    # "fielded" search configuration. Used by pulldown among other places.
+    # For supported keys in hash, see rdoc for Blacklight::SearchFields
+    #
+    # Search fields will inherit the :qt solr request handler from
+    # config[:default_solr_parameters], OR can specify a different one
+    # with a :qt key/value. Below examples inherit, except for subject
+    # that specifies the same :qt as default for our own internal
+    # testing purposes.
+    #
+    # The :key is what will be used to identify this BL search field internally,
+    # as well as in URLs -- so changing it after deployment may break bookmarked
+    # urls.  A display label will be automatically calculated from the :key,
+    # or can be specified manually to be different. 
+
+    # This one uses all the defaults set by the solr request handler. Which
+    # solr request handler? The one set in config[:default_solr_parameters][:qt],
+    # since we aren't specifying it otherwise. 
+    
+    config.add_search_field 'all_fields', :label => 'All Fields'
+    
+
+    # Now we see how to over-ride Solr request handler defaults, in this
+    # case for a BL "search field", which is really a dismax aggregate
+    # of Solr search fields. 
+    
+    config.add_search_field('title') do |field|
+      # solr_parameters hash are sent to Solr as ordinary url query params. 
+      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+
+      # :solr_local_parameters will be sent using Solr LocalParams
+      # syntax, as eg {! qf=$title_qf }. This is neccesary to use
+      # Solr parameter de-referencing like $title_qf.
+      # See: http://wiki.apache.org/solr/LocalParams
+      field.solr_local_parameters = { 
+        :qf => '$title_qf',
+        :pf => '$title_pf'
+      }
+    end
+    
+    config.add_search_field('author') do |field|
+      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+      field.solr_local_parameters = { 
+        :qf => '$author_qf',
+        :pf => '$author_pf'
+      }
+    end
+    
+    # Specifying a :qt only to show it's possible, and so our internal automated
+    # tests can test it. In this case it's the same as 
+    # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
+    config.add_search_field('subject') do |field|
+      field.solr_parameters = { :'spellcheck.dictionary' => 'subject' }
+      field.qt = 'search'
+      field.solr_local_parameters = { 
+        :qf => '$subject_qf',
+        :pf => '$subject_pf'
+      }
+    end
+
+    # "sort results by" select (pulldown)
+    # label in pulldown is followed by the name of the SOLR field to sort by and
+    # whether the sort is ascending or descending (it must be asc or desc
+    # except in the relevancy case).
+    config.add_sort_field 'score desc, pub_date_sort desc, title_sort asc', :label => 'relevance'
+    config.add_sort_field 'pub_date_sort desc, title_sort asc', :label => 'year'
+    config.add_sort_field 'author_sort asc, title_sort asc', :label => 'author'
+    config.add_sort_field 'title_sort asc, pub_date_sort desc', :label => 'title'
+
+    # If there are more than this many search results, no spelling ("did you 
+    # mean") suggestion is offered.
+    config.spell_max = 5
+  end
+
+
+
+end 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Admin::Application.config.secret_token = '4ac7ab0cba09df74f118a3d309031865075b29434ff23a19a9842c22b9a96acbd777d60077f2bb115d16f2321c6b2c11b4253fcf4fa8a8888a507d687291a1fe'
